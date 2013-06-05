82b9bf584b80ca1fa5aa9f52ac58b1ca2c6ee560
Chris <christian@isourcedigital.com>
2013-06-05 17:48:40 -0300

    retesteando mobileinit

--- js/custom.js
+++ js/custom.js
@@ -2,13 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
- $(document).on("mobileinit", function(){
-        alert("INICIA MOBILE")
-    });
+
 
 $(document).ready(function () {
     console.log("INICIA")
     
-   
+    $(document).on("mobileinit", function(){
+        alert("INICIA MOBILE")
+    });
    
 });
